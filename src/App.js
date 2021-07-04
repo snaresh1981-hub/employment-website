f94253c282d1eaeb52c0f0161c8bc1d4957c12e2
Naresh <naresh.shenishetty@gmail.com>
2021-07-04 20:33:12 +0530

    employment website

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return ( 
+    <div>
+      <div className="bg"> 
+         <h1 className="h1-responsive font-weight-bold">
+            Juniors make IT work.
+          </h1>
+          <h6 className="mb-4">
+            Hire and invest in highly skilled juniors now.
+          </h6>
+          <button type="button" class="btn btn-primary">
+            Post Offer Now
+          </button>  
+        </div> 
+      </div>
+  );
+}
+
+export default App;
